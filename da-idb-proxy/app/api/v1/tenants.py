970c2d7b0b620f175690dfe2093a9ae877b260bc
weir007 <42212616+weir007@users.noreply.github.com>
2026-03-27 16:59:29 +0800

    允许url为http

--- da-idb-proxy/app/api/v1/tenants.py
+++ da-idb-proxy/app/api/v1/tenants.py
@@ -20,7 +20,7 @@ ADMIN_USER = os.getenv("DEFAULT_TENANT_ADMIN_NAME", "tenant-admin")
 def _create_realm(realm: str, display_name: str):
     """1. 创建 Realm"""
     kc.request("POST", "/realms", json={
-        "realm": realm, "displayName": display_name, "enabled": True
+        "realm": realm, "displayName": display_name, "enabled": True, "sslRequired": "none"
     })
 
 
